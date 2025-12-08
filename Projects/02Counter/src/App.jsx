633cf52355ad4af1b4255d9a5d2d5f8fa0ed0f31
ompatil-711 <ompatilll.001@gmail.com>
2025-12-08 20:23:22 +0530

    Conter

---
+++ Projects/02Counter/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  const[counter,setCounter] = useState(15)
+
+  const addValue = () => {
+    console.log("Clicked",counter)
+    const newCounter  = counter +1
+    setCounter(newCounter)
+  }
+  const removeValue = () =>{
+    console.log("removed",counter)
+    const newCounter=counter-1
+    setCounter(newCounter)
+  }
+  return (
+    <>
+      <h1>Basic Project(Counter)</h1>
+      <h2>Counter value : {counter}</h2>
+      <button onClick={addValue}>Add Counter</button>
+      <br></br>
+      <button onClick={removeValue}>Remove Counter</button>
+    </>
+  )
+}
+
+export default App
