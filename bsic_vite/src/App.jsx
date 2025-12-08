a3ad041a788cb2f9a18cac375ce41fd8fe110600
ompatil-711 <ompatilll.001@gmail.com>
2025-12-08 14:37:54 +0530

    custom

--- bsic_vite/src/App.jsx
+++ bsic_vite/src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+
+import Chai from "./chai"
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const username = "chai aur code"
 
   return (
-    <h1>React+vite</h1>
-    
+    <>
+    <Chai/>
+    <h1>chai aur react {username}</h1>
+    <p>test para</p>
+   </>
   )
 }
 
