92e6907ef36e9f288fd7119a95ffba89f27a9c4a
ompatil-711 <ompatilll.001@gmail.com>
2025-12-09 18:35:10 +0530

    modification in counter

--- Projects/02Counter/src/App.jsx
+++ Projects/02Counter/src/App.jsx
@@ -9,12 +9,12 @@ function App() {
 
   const addValue = () => {
     console.log("Clicked",counter)
-    const newCounter  = counter +1
+    const newCounter  = counter +2
     setCounter(newCounter)
   }
   const removeValue = () =>{
     console.log("removed",counter)
-    const newCounter=counter-1
+    const newCounter=counter-2
     setCounter(newCounter)
   }
   return (
